d8bc19c983f6a4a3b7e11b5874a3d17ce68910cb
dameon green <dameongreen@gmail.com>
2023-08-24 21:59:36 -0400

    fix env

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,15 +25,28 @@ export default async function RootLayout({
         {
         layout: {
           helpPageUrl: "https://clerk.com/support",
-          logoImageUrl: "https://clerk.com/logo.png",
-          logoPlacement: "inside",
           privacyPageUrl: "https://clerk.com/privacy",
           showOptionalFields: true,
           socialButtonsPlacement: "bottom",
           socialButtonsVariant: "iconButton",
           termsPageUrl: "https://clerk.com/terms",
-            }
+            },
+             variables: { colorPrimary: "#000000" },
+          elements: {
+            formButtonPrimary:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
+            socialButtonsBlockButton:
+              "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+            socialButtonsBlockButtonText: "font-semibold",
+            formButtonReset:
+              "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+            membersPageInviteButton:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
+            card: "bg-[#fafafa]",
+          },
+
         }
+       
       }>
       <html lang="en">
         <body className={inter.className}>
